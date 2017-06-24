d60b214c643c2997c7a1c2ee8661b3ea7a92db63
benhsu75 <benhsu75@gmail.com>
2017-06-24 12:08:47 -0700

    Remove matplotlib stuff

--- model.py
+++ model.py
@@ -2,7 +2,6 @@ from __future__ import division, print_function, absolute_import
 
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
 
 # Import MNIST data
 from tensorflow.examples.tutorials.mnist import input_data
@@ -94,11 +93,3 @@ with tf.Session() as sess:
   # Applying encode and decode over test set
   encode_decode = sess.run(
       y_pred, feed_dict={X: mnist.test.images[:examples_to_show]})
-  # Compare original images with their reconstructions
-  f, a = plt.subplots(2, 10, figsize=(10, 2))
-  for i in range(examples_to_show):
-      a[0][i].imshow(np.reshape(mnist.test.images[i], (28, 28)))
-      a[1][i].imshow(np.reshape(encode_decode[i], (28, 28)))
-  f.show()
-  plt.draw()
-  plt.waitforbuttonpress()
